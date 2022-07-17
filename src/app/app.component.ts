302a476975c18a9af3add88194c71d7265f9207c
Edisson Lopez <edisonlopez1400@gmail.com>
2022-07-16 20:44:31 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-game';
+  // subscription: Subscription | undefined;
+  startButtonText = 'Comenzar';
+  hasGameStarted = false;
+  hasGameCounterFinish = false;
+  reactionTime = 0;
+  hasGameFinished = false;
+
+  startGame() {
+      this.reactionTime = 0;
+      this.startButtonText = 'PREPARATE'
+      this.hasGameFinished = false;
+      setTimeout(() => {
+        this,this.hasGameStarted = true;
+        this.reactionTime = new Date().getTime();
+      }, Math.floor(Math.random()*11)* 1000)
+
+    // this.subscription = interval(1000).subscribe(() => {
+    //   if(this.counterToStartGame === 0) {
+    //     this.hasGameCounterFinish = true;
+    //     this.reactionTime = new Date().getTime();        
+    //     return;
+    //   }
+    //   this.counterToStartGame = this.counterToStartGame-1
+    // });
+
+
+  }
+
+  finishGame() {
+
+    this.startButtonText = 'COMENZAR'
+    this.reactionTime = new Date().getTime() - this.reactionTime;
+    //this.subscription?.unsubscribe();
+    this.hasGameFinished = true;
+    //this.hasGameCounterFinish = false;
+    this.hasGameStarted = false; 
+
+  }
+
+  showResults() {
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  // Only For Modules
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
